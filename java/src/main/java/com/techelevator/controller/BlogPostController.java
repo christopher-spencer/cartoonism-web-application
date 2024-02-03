167b8a58b29982db511fcdf075247d76092d063b
thebellywolf <cdspenzer@gmail.com>
2024-02-03 10:21:26 -0500

    created getBlogPostById in Controller

--- java/src/main/java/com/techelevator/controller/BlogPostController.java
+++ java/src/main/java/com/techelevator/controller/BlogPostController.java
@@ -1,4 +1,24 @@
 package com.techelevator.controller;
 
+import com.techelevator.dao.BlogPostDao;
+import com.techelevator.model.BlogPost;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@PreAuthorize("isAuthenticated()")
 public class BlogPostController {
+
+    private BlogPostDao blogPostDao;
+
+    public BlogPostController(BlogPostDao blogPostDao) {
+        this.blogPostDao = blogPostDao;
+    }
+
+    @RequestMapping (path = "/blogposts/{blogPostId}", method = RequestMethod.GET)
+    public BlogPost getBlogPost(@PathVariable int blogPostId) {
+        BlogPost blogPost = blogPostDao.getBlogPostById(blogPostId);
+        return blogPost;
+    }
 }
