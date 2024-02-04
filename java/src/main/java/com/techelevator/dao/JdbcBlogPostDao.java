52758efddec0a97db33e88a27b1464d577061037
thebellywolf <cdspenzer@gmail.com>
2024-02-04 10:07:44 -0500

    added video blog option TODO

--- java/src/main/java/com/techelevator/dao/JdbcBlogPostDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcBlogPostDao.java
@@ -14,6 +14,8 @@ public class JdbcBlogPostDao implements BlogPostDao {
 
     private JdbcTemplate jdbcTemplate;
 
+    //TODO could be cool to add video blog option eventually as well
+
     //TODO Update the DataSource name to cartoonism for this project?
     public JdbcBlogPostDao(DataSource final_capstone) {
         this.jdbcTemplate = new JdbcTemplate(final_capstone);
