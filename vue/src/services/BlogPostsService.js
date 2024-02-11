6142f228e21992a5e7c070d95ca4091ff0f425f7
thebellywolf <cdspenzer@gmail.com>
2024-02-11 12:33:35 -0500

    added getBlogPost to blogpostservices

--- vue/src/services/BlogPostsService.js
+++ vue/src/services/BlogPostsService.js
@@ -4,6 +4,14 @@ export default {
 
     getBlogPosts() {
         return axios.get('/blogposts');
-    }
+    },
+
+    getBlogPost(blogPostId) {
+        return axios.get('/blogposts/${blogPostId}');
+    },
+
+    
+
+
 
 }
