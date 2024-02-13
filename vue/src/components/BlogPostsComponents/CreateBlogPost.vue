85c8c49edff302a99b7a42de769c27e224224802
thebellywolf <cdspenzer@gmail.com>
2024-02-13 18:23:02 -0500

    working on createBlogPost method promise

--- vue/src/components/BlogPostsComponents/CreateBlogPost.vue
+++ vue/src/components/BlogPostsComponents/CreateBlogPost.vue
@@ -13,13 +13,35 @@ export default {
     return {
       showForm: false,
       createdBlogPost: {
-
+        id: "",
+        name: "",
+        author: "",
+        description: "",
+        content: "",
+        imageName: "",
+        imageUrl: "",
+        createdAt: "",
+        updatedAt: ""
       }
     }
   },
   computed: {
     isFormValid() {
       return this.createdBlogPost.name;
+    }
+  },
+  methods: {
+    createBlogPost() {
+      this.id = this.$store.state.user.id;
+
+      BlogPostsService.addBlogPost(this.createBlogPost)
+        .then((response) => {
+          if (response.status === 201) {
+            if (response.data) {
+              this.createBlogPost = response.data;
+            }
+          }
+        });
     }
   }
 }
