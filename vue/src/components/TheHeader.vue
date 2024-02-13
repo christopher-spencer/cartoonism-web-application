dde2547b0b0b23561e971269621d3923a19516c9
thebellywolf <cdspenzer@gmail.com>
2024-02-13 12:11:42 -0500

    created header and footer view

--- vue/src/components/TheHeader.vue
+++ vue/src/components/TheHeader.vue
@@ -1,5 +1,22 @@
 <template>
-  <div></div>
+  <div class="header-container">
+    <nav class="nav-container">
+        <ul class="nav-list">
+            <li class="nav-item">
+
+            </li>
+            <li class="nav-item">
+                
+            </li>
+            <li class="nav-item">
+                
+            </li>
+            <li class="nav-item">
+                
+            </li>
+        </ul>
+    </nav>
+  </div>
 </template>
 
 <script>
