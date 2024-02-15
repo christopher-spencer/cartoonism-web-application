313d97861a02b420a111d1d8b6c821eee7db4e7f
thebellywolf <cdspenzer@gmail.com>
2024-02-15 12:45:21 -0500

    working on editBlogPost

--- vue/src/components/BlogPostsComponents/EditBlogPost.vue
+++ vue/src/components/BlogPostsComponents/EditBlogPost.vue
@@ -1,10 +1,28 @@
 <template>
-  <div></div>
+  <div id="edit-blog-post">
+    <div class="edit-blog-post-form">
+
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: "edit-blog-post",
+  data() {
+    return {
+      showForm: false,
+      editedBlogPost: {
+        id: '',
+        name: '',
+        author: '',
+        description: '',
+        content: '',
+        imageName: '',
+        imageUrl: ''
+      }
+    };
+  }
 }
 </script>
 
