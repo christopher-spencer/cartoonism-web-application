f902e8fca6245f033a848e975f19d4b2d3bb8219
thebellywolf <cdspenzer@gmail.com>
2024-02-15 11:46:23 -0500

    added BlogPostView file to views

---
+++ vue/src/views/BlogPostsView/BlogPostView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <BlogPost/>
+    </div>
+  </template>
+  
+  <script>
+  import BlogPost from '../../components/BlogPostsComponents/BlogPost.vue';
+  export default {
+      components: {
+        BlogPost
+      }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
