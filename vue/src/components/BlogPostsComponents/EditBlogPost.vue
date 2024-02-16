a82b760f9ebf06d9d547f038a546923d8873bb90
thebellywolf <cdspenzer@gmail.com>
2024-02-16 15:40:43 -0500

    working on edit blog post component

--- vue/src/components/BlogPostsComponents/EditBlogPost.vue
+++ vue/src/components/BlogPostsComponents/EditBlogPost.vue
@@ -7,6 +7,8 @@
 </template>
 
 <script>
+import BlogPostsService from '../../services/BlogPostsService';
+
 export default {
   name: "edit-blog-post",
   data() {
@@ -33,7 +35,53 @@ export default {
     }
   },
   methods: {
-    
+    getBlogPostData(blogPostId) {
+      BlogPostsService.getBlogPost(blogPostId)
+        .then((response) => {
+          this.editedBlogPost = response.data;
+        })
+        .catch(function (error) {
+          if (error.response) {
+            if (error.response.status === 404) {
+              alert(error.response.data.message);
+            }
+          }
+        });
+    }, 
+    editBlogPost() {
+      BlogPostsService.updateBlogPost(this.editedBlogPost.id)
+        .then((response) => {
+          if (response.status === 200) {
+            if (response.data) {
+              this.editBlogPost = response.data;
+              // this.$store.commit("UPDATE_BLOGPOST", this.editedBlogPost);
+            } else {
+              console.error("Response data is missing 'id' property:", response.data);
+            }
+          } else {
+            console.error("Unexpected status code:", response.status);
+          }
+        })
+        .catch((err) => console.error("API Error:", err));
+    },
+    resetForm() {
+      this.editedBlogPost = {
+        id: '',
+        name: '',
+        author: '',
+        description: '',
+        content: '',
+        imageName: '',
+        imageUrl: ''
+      };
+    },
+    hideForm() {
+      this.showForm = false;
+      this.resetForm();
+    }
+  },
+  created() {
+    this.getBlogPostData(this.$route.params.id);
   }
 }
 </script>
