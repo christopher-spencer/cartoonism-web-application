6e2772b10474cf644ec9ad359a69aedef682cf68
thebellywolf <cdspenzer@gmail.com>
2024-02-17 11:28:31 -0500

    working on delete blog post component

--- vue/src/components/BlogPostsComponents/DeleteBlogPost.vue
+++ vue/src/components/BlogPostsComponents/DeleteBlogPost.vue
@@ -1,10 +1,17 @@
 <template>
-  <div></div>
+  <div class="delete-button-container">
+    <button class="delete-button" v-on:click="deleteBlogPost(blogPost.id)">Delete Blog Post</button>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: "DeleteBlogPost",
+  data() {
+    return {
+      blogPost: {}
+    }
+  }
 }
 </script>
 
